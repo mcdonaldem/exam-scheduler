ba1a73d669a3158343a3413e731ea838ba8265ec
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-06 11:59:36 +0100

    Add Student model class

--- ExamScheduler/Models/Student.cs
+++ ExamScheduler/Models/Student.cs
@@ -2,5 +2,9 @@
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+        public List<Exam> Exams { get; set; }
     }
 }
