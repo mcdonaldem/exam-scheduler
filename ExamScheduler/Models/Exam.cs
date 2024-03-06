d0277103af52a1bf3eda32adfc94c40e50ab5f80
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-06 11:57:56 +0100

    Add Exam model class

--- ExamScheduler/Models/Exam.cs
+++ ExamScheduler/Models/Exam.cs
@@ -2,5 +2,10 @@
 {
     public class Exam
     {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public List<Mentor> Mentors { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
     }
 }
