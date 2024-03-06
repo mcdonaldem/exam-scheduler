672a51685c7fe09b77e979d63842c6333d113236
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-06 11:58:29 +0100

    Add Enrollment model class

---
+++ ExamScheduler/Models/Enrollment.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamScheduler.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
