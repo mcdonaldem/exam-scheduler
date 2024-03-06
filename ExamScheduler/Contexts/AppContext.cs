c392a73aa7c9fdd7115e750dcd7194196ad1cdd5
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-06 12:23:03 +0100

    Add db context file & list tables

---
+++ ExamScheduler/Contexts/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using ExamScheduler.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamScheduler.Contexts
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Mentor> Mentors { get; set; }
+        public DbSet<MentorAvailability> MentorAvailabilities { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
