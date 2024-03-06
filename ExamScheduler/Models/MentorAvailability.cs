dc9c28161757682f1f9fb667e1327d7572c21393
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-06 11:59:52 +0100

    Add Mentor Availability model class

--- ExamScheduler/Models/MentorAvailability.cs
+++ ExamScheduler/Models/MentorAvailability.cs
@@ -1,6 +1,12 @@
-﻿namespace ExamScheduler.Models
+﻿using ExamScheduler.Models.Enums;
+
+namespace ExamScheduler.Models
 {
     public class MentorAvailability
     {
+        public int Id { get; set; }
+        public Mentor Mentor { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeSlot TimeSlot { get; set; }
     }
 }
