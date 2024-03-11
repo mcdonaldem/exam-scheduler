5dd4ea0acc5072380acd4d951c7388c91d18d0a8
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-11 12:07:11 +0100

    Add GetAllActive()

--- ExamScheduler/Services/MentorService.cs
+++ ExamScheduler/Services/MentorService.cs
@@ -1,6 +1,23 @@
-﻿namespace ExamScheduler.Services
+﻿using ExamScheduler.Contexts;
+using ExamScheduler.Models;
+
+namespace ExamScheduler.Services
 {
     public class MentorService
     {
+        private ApplicationContext _context;
+
+        public MentorService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Mentor> GetAllActive()
+        {
+            return _context.Mentors
+                .Where(m => m.IsActive)
+                .ToList()
+                ;
+        }
     }
 }
