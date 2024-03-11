95cb6b6e438a2642fcaf518bcdbed291e373f4c3
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-11 18:39:35 +0100

    Add explicit foreign keys

--- ExamScheduler/Models/Enrollment.cs
+++ ExamScheduler/Models/Enrollment.cs
@@ -1,9 +1,13 @@
-﻿namespace ExamScheduler.Models
+﻿using System.Numerics;
+
+namespace ExamScheduler.Models
 {
     public class Enrollment
     {
         public int Id { get; set; }
+        public int StudentId { get; set; }
         public Student Student { get; set; }
+        public int CourseId { get; set; }
         public Course Course { get; set; }
     }
 }
