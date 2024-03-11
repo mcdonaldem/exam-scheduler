746f51c4d6d80f09a851f0a387fcf5ef4f8eb162
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-11 12:04:20 +0100

    Create custom mentor data parsing exception

---
+++ ExamScheduler/Exceptions/InvalidMentorDataException.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamScheduler.Exceptions
+{
+    public class InvalidMentorDataException : Exception
+    {
+        public InvalidMentorDataException(string? message) : base(message)
+        {
+        }
+    }
+}
