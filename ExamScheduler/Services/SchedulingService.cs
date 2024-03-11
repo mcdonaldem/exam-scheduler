56a0bebffb342a24129daa21e5a3595107909fe2
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-11 12:08:43 +0100

    Add converter method for start times

---
+++ ExamScheduler/Services/SchedulingService.cs
@@ -0,0 +1,14 @@
+﻿using ExamScheduler.Models.Enums;
+
+namespace ExamScheduler.Services
+{
+    public class SchedulingService
+    {
+        public TimeOnly GetStartTime(TimeSlot timeSlot) => timeSlot switch
+        {
+            TimeSlot.Morning => TimeOnly.Parse(Environment.GetEnvironmentVariable("MORNING_START")),
+            TimeSlot.EarlyAfternoon => TimeOnly.Parse(Environment.GetEnvironmentVariable("EARLY_AFTERNOON_START")),
+            TimeSlot.LateAfternoon => TimeOnly.Parse(Environment.GetEnvironmentVariable("LATE_AFTERNOON_START"))
+        };
+    }
+}
