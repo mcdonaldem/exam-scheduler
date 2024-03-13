689670241d21dab0319a8a52efc79af9ef5e7f9d
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-13 16:56:54 +0100

    Add first endpoint

--- ExamScheduler/Controllers/HomeController.cs
+++ ExamScheduler/Controllers/HomeController.cs
@@ -1,6 +1,36 @@
-﻿namespace ExamScheduler.Controllers
+﻿using ExamScheduler.Entities;
+using ExamScheduler.Exceptions;
+using ExamScheduler.Models.Enums;
+using ExamScheduler.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamScheduler.Controllers
 {
-    public class HomeController
+    public class HomeController(OutputSerializerService outputSerializerService, SchedulingService schedulingService, IConfiguration configuration) : Controller
     {
+        private OutputSerializerService outputSerializerService = outputSerializerService;
+        private SchedulingService schedulingService = schedulingService;
+        private IConfiguration configuration = configuration;
+
+        [HttpGet("/schedule")]
+        public IActionResult GetFile(IFormFile mentorInfo, IFormFile studentInfo, int courseId)
+        {
+            var content = schedulingService.CreateSchedule(mentorInfo, studentInfo, courseId);
+
+            try
+            {
+                Func<List<Exam>, dynamic> output = (OutputSerialzation)int.Parse(configuration["OutputSerialization"]) switch
+                {
+                    OutputSerialzation.ByteArray => outputSerializerService.ToByteArray,
+                    OutputSerialzation.Stream => outputSerializerService.ToStream,
+                    _ => throw new ConfigurationException("Output serialization type not recognized.")
+                };
+                return File(output(content), "text/csv", "scheduled_exams.csv");
+            }
+            catch (Exception e)
+            {
+                throw new ConfigurationException("Output serialization type not configured.", e);
+            }
+        }
     }
 }
---
+++ ExamScheduler/Exceptions/ConfigurationException.cs
@@ -0,0 +1,13 @@
+﻿namespace ExamScheduler.Exceptions
+{
+    public class ConfigurationException : Exception
+    {
+        public ConfigurationException(string? message) : base(message)
+        {
+        }
+
+        public ConfigurationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
