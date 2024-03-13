c446f71df94d6f21b6873d49a4e595545aab38b5
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-13 15:52:51 +0100

    Add output serialization methods

---
+++ ExamScheduler/Services/OutputSerializerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using ExamScheduler.Entities;
+
+namespace ExamScheduler.Services
+{
+    public class OutputSerializerService
+    {
+        public byte[] ToByteArray(List<Exam> exams)
+        {
+            return Encoding.UTF32.GetBytes(String.Join(Environment.NewLine, exams.Select(e => e.ToFileString())));
+        }
+
+        public Stream ToStream(List<Exam> exams)
+        {
+            return new MemoryStream(ToByteArray(exams));
+        }
+    }
+}
