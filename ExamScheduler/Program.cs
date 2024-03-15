9eb440a9d42c1b4b196b717a13d655c66fd8fd51
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-15 14:11:40 +0100

    Register all services

--- ExamScheduler/Program.cs
+++ ExamScheduler/Program.cs
@@ -1,7 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ExamScheduler.Contexts;
+using ExamScheduler.Services.Interfaces;
+using ExamScheduler.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IMentorService, MentorService>();
+builder.Services.AddScoped<IOutputSerializerService, OutputSerializerService>();
+builder.Services.AddScoped<IParsingService, ParsingService>();
+builder.Services.AddScoped<ISchedulingService, SchedulingService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
 
 if (builder.Environment.IsDevelopment())
 {
