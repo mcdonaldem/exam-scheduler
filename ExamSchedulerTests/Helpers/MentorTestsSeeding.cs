8293b52bcccbe3855023bc809bb05dcb068e0951
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-03-18 15:55:34 +0100

    Add unit testing helpers

---
+++ ExamSchedulerTests/Helpers/MentorTestsSeeding.cs
@@ -0,0 +1,59 @@
+﻿using ExamScheduler.Contexts;
+using ExamScheduler.Entities;
+
+namespace ExamSchedulerTests.Helpers
+{
+    public static class MentorTestsSeeding
+    {
+        public static ApplicationContext Seed(ApplicationContext context)
+        {
+            // Seed algo languages
+            List<AlgoLanguage> algoLangs = [
+                new AlgoLanguage("Java"),
+                new AlgoLanguage("Python"),
+                new AlgoLanguage("TypeScript"),
+                new AlgoLanguage("C#")
+            ];
+
+            context.AlgoLanguages
+                .AddRange(algoLangs)
+                ;
+            context.SaveChanges();
+
+            var langs = context.AlgoLanguages
+                .ToList()
+                ;
+
+            // Seed Mentors
+            List<Mentor> mentors = [
+                new Mentor("Arthur", true),
+                new Mentor("Beatrice", true),
+                new Mentor("Calvin", false),
+                new Mentor("Delia", true),
+                new Mentor("Ethan", true),
+                new Mentor("Fiona", false),
+                new Mentor("George", true),
+                new Mentor("Hanna", true)
+            ];
+
+            foreach(var m in mentors)
+            {
+                var knownLangs = new Random().Next(1, langs.Count + 1);
+                var indices = Enumerable
+                    .Range(0, langs.Count)
+                    .ToList()
+                    ;
+                for (int i = 1; i <= knownLangs; i++)
+                {
+                    var randIndex = new Random().Next(indices.Count());
+                    m.AlgoLanguages.Add(langs[indices[randIndex]]);
+                    indices.RemoveAt(randIndex);
+                }
+            }
+
+            context.Mentors.AddRange(mentors);
+            context.SaveChanges();
+            return context;
+        }
+    }
+}
---
+++ ExamSchedulerTests/TestSetup/TestsBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Common;
+using ExamScheduler.Contexts;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamSchedulerTests.TestSetup
+{
+    public class TestsBase : IDisposable
+    {
+        private DbConnection? connection;
+        private DbContextOptions<ApplicationContext>? contextOptions;
+
+        public ApplicationContext GetSqliteDbContext()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            contextOptions = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            var context = new ApplicationContext(contextOptions);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        public void Dispose()
+        {
+            connection?.Dispose();
+        }
+    }
+}
