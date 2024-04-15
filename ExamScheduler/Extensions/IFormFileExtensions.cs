d57640db48fe616660a60fba5d0bb4cfb9b5008e
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-04-15 18:06:24 +0200

    Change text encoding due to found bug

--- ExamScheduler/Extensions/IFormFileExtensions.cs
+++ ExamScheduler/Extensions/IFormFileExtensions.cs
@@ -4,11 +4,9 @@
     {
         public static string[] ReadAsArray(this IFormFile file)
         {
-            using (var fileStream = file.OpenReadStream())
+            using (var reader = new StreamReader(file.OpenReadStream()))
             {
-                var bytes = new byte[file.Length];
-                fileStream.Read(bytes, 0, (int)file.Length);
-                return Convert.ToBase64String(bytes)
+                return reader.ReadToEnd()
                     .Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
                     ;
             }
